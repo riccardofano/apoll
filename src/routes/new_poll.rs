a4b530e9b514210554f6bd793a2cb5e3b9555dde
riccardofano <rcr.fano@gmail.com>
2022-06-06 16:51:03 +0200

    accept form data

--- src/routes/new_poll.rs
+++ src/routes/new_poll.rs
@@ -1,5 +1,10 @@
 use actix_web::HttpResponse;
 
+pub struct FormData {
+    pub username: String,
+    pub prompt: String,
+}
+
 pub async fn create_poll() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
--- tests/api/new_poll.rs
+++ tests/api/new_poll.rs
@@ -1,12 +1,20 @@
+use uuid::Uuid;
+
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn create_poll_returns_status_200() {
+async fn create_poll_returns_200_with_valid_form_data() {
     let app = TestApp::new().await;
     let client = reqwest::Client::new();
 
+    let body = serde_json::json!({
+        "username": Uuid::new_v4().to_string(),
+        "prompt": Uuid::new_v4().to_string(),
+    });
+
     let response = client
         .post(app.endpoint("/new"))
+        .form(&body)
         .send()
         .await
         .expect("failed to execute request");
