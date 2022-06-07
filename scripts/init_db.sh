d274c2cd1f34bb3aa055d473b8013936a364769c
riccardofano <63453504+riccardofano@users.noreply.github.com>
2022-06-07 22:18:52 +0200

    Add general ci routine (#2)
    * add general ci routine
    * use `cargo sqlx prepare --merged` instead of `--bin apoll` to avoid stub sqlx-data.json files

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,15 +7,27 @@ DB_PASSWORD=${POSTGRES_PASSWORD:=password}
 DB_NAME=${POSTGRES_DB:=apoll}
 DB_PORT=${POSTGRES_PORT:=5432}
 
-# run docker server
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -e POSTGRES_PORT=${DB_PORT} \
-    -d postgres \
-    # increase max number of connections for testing
-    postgres -N 1000
+# Set SKIP_DOCKER to skip Docker if a Postgres db is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+    if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+        echo >&2 "there is a postgres container alraedy running, kill it with"
+        echo >&2 "  docker kill ${RUNNING_POSTGRES_CONTAINER}"
+        exit 1
+    fi
+
+    # run docker server
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -e POSTGRES_PORT=${DB_PORT} \
+        -d \
+        --name "postgres_$(date '+%s')" \
+        # increase max number of connections for testing
+        postgres -N 1000
+fi
 
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
@@ -27,3 +39,6 @@ done
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
