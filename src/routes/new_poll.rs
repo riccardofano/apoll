45d085c17f0563d3ae890bc7ed01904e2f69ea75
riccardofano <rcr.fano@gmail.com>
2022-06-07 10:54:16 +0200

    add create poll error enum

--- src/routes/new_poll.rs
+++ src/routes/new_poll.rs
@@ -1,13 +1,28 @@
-use actix_web::{web, HttpResponse};
-use validator::Validate;
+use actix_web::{web, HttpResponse, ResponseError};
+use reqwest::StatusCode;
+use validator::{Validate, ValidationErrors};
 
 use crate::domain::PollFormData;
 
-pub async fn create_poll(form: web::Form<PollFormData>) -> HttpResponse {
-    let _form = match form.validate() {
-        Ok(form) => form,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
+#[derive(thiserror::Error, Debug)]
+pub enum CreatePollError {
+    #[error("{0}")]
+    ValidationError(ValidationErrors),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
 
-    HttpResponse::Ok().finish()
+impl ResponseError for CreatePollError {
+    fn status_code(&self) -> reqwest::StatusCode {
+        match self {
+            CreatePollError::ValidationError(_) => StatusCode::BAD_REQUEST,
+            CreatePollError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+pub async fn create_poll(form: web::Form<PollFormData>) -> Result<HttpResponse, CreatePollError> {
+    let _form = form.validate().map_err(CreatePollError::ValidationError)?;
+
+    Ok(HttpResponse::Ok().finish())
 }
