5e21a78471cfc34ddaa5e1ca7442ac854a28d7f5
riccardofano <rcr.fano@gmail.com>
2022-06-08 10:31:54 +0200

    add tracing support to tests

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,12 +1,27 @@
 use std::net::TcpListener;
 
-use apoll::{
-    configuration::{DatabaseSettings, Settings},
-    startup::run,
-};
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use tokio::runtime::Runtime;
 use uuid::Uuid;
+
+use apoll::configuration::{DatabaseSettings, Settings};
+use apoll::startup::run;
+use apoll::telemetry::{get_subscriber, init_subscriber};
+
+// Only initialize tracing once
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -16,6 +31,9 @@ pub struct TestApp {
 
 impl TestApp {
     pub async fn new() -> Self {
+        // Start tracing
+        Lazy::force(&TRACING);
+
         // Assign a random name and port to the application
         let configuration = {
             let mut c = Settings::new().expect("failed to read configuration");
