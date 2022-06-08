1e1a5eafac9681fd12eef4472ef0c4628b6ae5b2
riccardofano <rcr.fano@gmail.com>
2022-06-08 14:52:49 +0200

    validate if poll_id exists

--- src/routes/poll/get.rs
+++ src/routes/poll/get.rs
@@ -1,6 +1,33 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
 use uuid::Uuid;
 
-pub async fn show_poll(path: web::Path<Uuid>) -> HttpResponse {
-    HttpResponse::Ok().body(path.into_inner().to_string())
+pub async fn show_poll(path: web::Path<Uuid>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let poll_id = path.into_inner();
+    let _ = match validate_poll_id(&db_pool, poll_id).await {
+        Ok(p) => match p {
+            Some(p) => p,
+            None => return HttpResponse::NotFound().finish(),
+        },
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+    HttpResponse::Ok().body(poll_id.to_string())
+}
+
+pub async fn validate_poll_id(
+    db_pool: &PgPool,
+    poll_id: Uuid,
+) -> Result<Option<String>, sqlx::Error> {
+    let result = sqlx::query!(
+        r#"
+        SELECT prompt
+        FROM polls
+        WHERE poll_id = $1
+        "#,
+        poll_id
+    )
+    .fetch_optional(db_pool)
+    .await?;
+
+    Ok(result.map(|r| r.prompt))
 }
