bff6b353ac4169db73a0d0280d1ccfe36d1d42df
riccardofano <rcr.fano@gmail.com>
2022-06-09 15:41:41 +0200

    add error type to show poll

--- src/routes/poll/get.rs
+++ src/routes/poll/get.rs
@@ -1,17 +1,38 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, ResponseError};
+use anyhow::Context;
+use reqwest::StatusCode;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-pub async fn show_poll(path: web::Path<Uuid>, db_pool: web::Data<PgPool>) -> HttpResponse {
+#[derive(thiserror::Error, Debug)]
+pub enum ShowPollError {
+    #[error("could not find poll")]
+    InvalidPollError,
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl ResponseError for ShowPollError {
+    fn status_code(&self) -> reqwest::StatusCode {
+        match self {
+            ShowPollError::InvalidPollError => StatusCode::NOT_FOUND,
+            ShowPollError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+pub async fn show_poll(
+    path: web::Path<Uuid>,
+    db_pool: web::Data<PgPool>,
+) -> Result<HttpResponse, ShowPollError> {
     let poll_id = path.into_inner();
     let _ = match validate_poll_id(&db_pool, poll_id).await {
-        Ok(p) => match p {
-            Some(p) => p,
-            None => return HttpResponse::NotFound().finish(),
-        },
-        Err(_) => return HttpResponse::InternalServerError().finish(),
-    };
-    HttpResponse::Ok().body(poll_id.to_string())
+
+    let prompt = validate_poll_id(&db_pool, poll_id)
+        .await
+        .context("failed to query database for poll_id")?
+        .ok_or(ShowPollError::InvalidPollError)?;
+
+    Ok(HttpResponse::Ok().body(prompt))
 }
 
 pub async fn validate_poll_id(
