b777c4d8961891be61081f07f3ed40b9b15cb8f1
riccardofano <rcr.fano@gmail.com>
2022-06-13 18:33:51 +0200

    create and insert joining user to the poll

--- src/routes/poll/post_join.rs
+++ src/routes/poll/post_join.rs
@@ -37,19 +37,23 @@ pub struct JoinForm {
 pub async fn join_poll(
     path: web::Path<Uuid>,
     form: web::Form<JoinForm>,
-    db_poll: web::Data<PgPool>,
+    db_pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, JoinError> {
     let poll_id = path.into_inner();
     tracing::Span::current().record("user_id", &tracing::field::display(&poll_id));
     tracing::Span::current().record("user_name", &tracing::field::display(&form.0.username));
 
-    let prompt = validate_poll_id(&db_poll, poll_id)
+    let prompt = validate_poll_id(&db_pool, poll_id)
         .await
         .context("failed to retrieve poll_id")?;
 
     if prompt.is_none() {
         return Err(JoinError::NotFoundError);
     }
+
+    create_and_insert_user(&db_pool, poll_id, form.0.username)
+        .await
+        .context("failed to create and insert user into the poll")?;
 
     Ok(HttpResponse::Ok().finish())
 }
@@ -72,4 +76,43 @@ async fn validate_poll_id(db_poll: &PgPool, poll_id: Uuid) -> Result<Option<Stri
     .await?;
 
     Ok(result.map(|r| r.prompt))
+}
+
+#[tracing::instrument(
+    name = "create and insert user into poll users"
+    skip(db_pool)
+)]
+async fn create_and_insert_user(
+    db_pool: &PgPool,
+    poll_id: Uuid,
+    username: String,
+) -> Result<(), sqlx::Error> {
+    let mut transaction = db_pool.begin().await?;
+
+    let user_id = Uuid::new_v4();
+    sqlx::query!(
+        r#"
+        INSERT INTO users (user_id, created_at)
+        VALUES ($1, now())
+        "#,
+        &user_id
+    )
+    .execute(&mut transaction)
+    .await?;
+
+    sqlx::query!(
+        r#"
+        INSERT INTO poll_users (poll_id, user_id, username)
+        VALUES ($1, $2, $3)
+        "#,
+        poll_id,
+        &user_id,
+        username
+    )
+    .execute(&mut transaction)
+    .await?;
+
+    transaction.commit().await?;
+
+    Ok(())
 }
